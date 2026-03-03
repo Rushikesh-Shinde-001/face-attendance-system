9e7db9fb5c19de30b2e2d29d0ff3f4f702babe30
Rushikesh Shinde <shinderushikesh306@gmail.com>
2026-03-03 20:51:47 +0530

    Register Department in admin panel

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Department, Student, Attendance
 
-# Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'code']
+    search_fields = ['name', 'code']
+    list_filter = ['name']
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['student_id', 'name', 'email', 'department', 'year', 'can_mark_attendance']
+    search_fields = ['student_id', 'name', 'email']
+    list_filter = ['department', 'year', 'can_mark_attendance']
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ['student', 'date', 'time']
+    search_fields = ['student__name', 'student__student_id']
+    list_filter = ['date']
